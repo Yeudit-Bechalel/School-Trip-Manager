646345ca7705068cdc220b53075d96189dbd6281
Yeudit-Bechalel <yb0583256120@gmail.com>
2026-04-26 16:20:51 +0300

    Successfully connected to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("השרת של הטיול");
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); // שורת הקסם שקוראת את הסודות מקובץ ה-.env
+
+const app = express();
+const port = 5000;
+
+// הגדרת חיבור למסד הנתונים בענן
+// אנחנו משתמשים בקישור שהחבאנו ב-process.env.MONGO_URI
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ Connected to MongoDB Atlas!'))
+    .catch((err) => console.log('❌ Failed to connect to MongoDB', err));
+
+// נתיב בדיקה כדי לראות שהשרת מגיב
+app.get('/', (req, res) => {
+    res.send('ברוכה הבאה לשרת הטיול השנתי של בנות משה!');
+});
+
+// הפעלת השרת
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
